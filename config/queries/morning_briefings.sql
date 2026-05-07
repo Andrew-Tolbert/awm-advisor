90b1d8bcd0876774b6cb3a6df161a94f12ed2826
Andrew-Tolbert <andrew.tolbert@databricks.com>
2026-05-06 21:01:04 -0400

    Update morning briefings SQL query to select the latest briefing date
    - Modified the WHERE clause to retrieve the most recent briefing date using a subquery, enhancing data accuracy for advisor-specific briefings.

--- config/queries/morning_briefings.sql
+++ config/queries/morning_briefings.sql
@@ -6,7 +6,7 @@ SELECT
   status,
   generated_at
 FROM ahtsa.awm.app_morning_briefings
-WHERE briefing_date = current_date()
+WHERE briefing_date = (SELECT MAX(briefing_date) FROM ahtsa.awm.app_morning_briefings)
   AND advisor_id = :advisor_id
   AND section_id IS NOT NULL
 ORDER BY section_id
